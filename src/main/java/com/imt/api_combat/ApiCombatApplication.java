99500591e85af48c5c9708679018f113c940a5ce
mathismr <mathis10r@outlook.fr>
2026-02-06 19:37:56 +0100

    refactor part2

---
+++ src/main/java/com/imt/api_combat/ApiCombatApplication.java
@@ -0,0 +1,13 @@
+package com.imt.api_combat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCombatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCombatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imt/api_combat/ApiCombatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imt.api_combat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCombatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

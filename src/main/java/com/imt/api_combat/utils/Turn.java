af8af28357d134339eae3a35be7901059a7ab59b
mathismr <mathis10r@outlook.fr>
2026-02-23 22:35:21 +0100

    wip

--- src/main/java/com/imt/api_combat/utils/Turn.java
+++ src/main/java/com/imt/api_combat/utils/Turn.java
@@ -1,9 +1,14 @@
 package com.imt.api_combat.utils;
 
+import java.util.List;
+import java.util.UUID;
+
 public class Turn {
     private final int id;
+    private final List<UUID> monsters;
 
-    public Turn(int id) {
+    public Turn(int id, List<UUID> monsters) {
         this.id = id;
+        this.monsters = monsters;
     }
 }

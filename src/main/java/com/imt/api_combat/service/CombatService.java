18034442371a1ae44dfbafb11ea3aa869d5a98d9
mathismr <mathis10r@outlook.fr>
2026-02-23 19:32:59 +0100

    func: registerCombat in CombatService

--- src/main/java/com/imt/api_combat/service/CombatService.java
+++ src/main/java/com/imt/api_combat/service/CombatService.java
@@ -5,6 +5,7 @@ import com.imt.api_combat.persistence.dto.CombatMongoDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -14,5 +15,16 @@ public class CombatService {
 
     public CombatMongoDTO getCombat(String id) {
         return combatMongoDAO.findbyId(UUID.fromString(id));
+    }
+
+    public UUID registerCombat(List<UUID> monsters, List<String> players) {
+        UUID id = UUID.randomUUID();
+        try {
+            CombatMongoDTO combatMongoDTO = new CombatMongoDTO(id, monsters, players);
+            combatMongoDAO.save(combatMongoDTO);
+        } catch (Exception e) {
+            return null;
+        }
+        return id;
     }
 }

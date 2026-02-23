1db773b81cce331af576bde1f2e8db528adb7475
mathismr <mathis10r@outlook.fr>
2026-02-23 19:33:20 +0100

    Add controller DTOs

--- src/main/java/com/imt/api_combat/controller/dto/input/NewCombatHttpRequest.java
+++ src/main/java/com/imt/api_combat/controller/dto/input/NewCombatHttpRequest.java
@@ -1,4 +1,14 @@
 package com.imt.api_combat.controller.dto.input;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
 public class NewCombatHttpRequest {
+    private List<UUID> monsters;
+    private List<String> players;
 }
--- src/main/java/com/imt/api_combat/controller/dto/output/NewCombatHttpResponse.java
+++ src/main/java/com/imt/api_combat/controller/dto/output/NewCombatHttpResponse.java
@@ -1,4 +1,14 @@
 package com.imt.api_combat.controller.dto.output;
 
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class NewCombatHttpResponse {
+    private UUID combatId;
+
+    public NewCombatHttpResponse(UUID combatId) {
+        this.combatId = combatId;
+    }
 }
